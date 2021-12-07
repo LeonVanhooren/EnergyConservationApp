092c33be785c5a784712951e55ca87abb86e7d6f
thodnys <thomas.denys@ugent.be>
2021-12-07 17:52:33 +0100

    nieuwe versie

--- src/main/java/com/app/energyconservationapp/guiController.java
+++ src/main/java/com/app/energyconservationapp/guiController.java
@@ -2,21 +2,17 @@ package com.app.energyconservationapp;
 
 import Objects.*;
 import databaseTest.dbTest;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.w3c.dom.events.Event;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
